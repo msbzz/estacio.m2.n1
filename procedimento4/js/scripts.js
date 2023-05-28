ba589b8b232839b8044630cada5ec2180e9f2872
Marco <mbarozzi@gmail.com>
2023-05-28 18:25:29 -0300

    Procedimento | Finalização do Site

---
+++ procedimento4/js/scripts.js
@@ -0,0 +1,9 @@
+function verificarTamanhoPagina() {
+    // Obtenha o comprimento atual da página
+    var comprimentoAtual = window.innerWidth;
+    var alturaAtual = window.innerHeight;
+    alert("A largura da sua página é de " + comprimentoAtual + " e altura de " + alturaAtual + " pixels!");
+  }
+  
+  verificarTamanhoPagina()
+  
